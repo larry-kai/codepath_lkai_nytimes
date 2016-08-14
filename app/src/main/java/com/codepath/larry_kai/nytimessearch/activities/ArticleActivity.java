35852a88906e5e84f2685d520fcd60a23c5a7b4f
Larry Kai <larry.kai@airbnb.com>
2016-08-14 16:32:15 -0700

    add sharing with friends

--- app/src/main/java/com/codepath/larry_kai/nytimessearch/activities/ArticleActivity.java
+++ app/src/main/java/com/codepath/larry_kai/nytimessearch/activities/ArticleActivity.java
@@ -1,13 +1,18 @@
 package com.codepath.larry_kai.nytimessearch.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.ShareActionProvider;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
-import com.codepath.larry_kai.nytimessearch.models.Article;
 import com.codepath.larry_kai.nytimessearch.R;
+import com.codepath.larry_kai.nytimessearch.models.Article;
 
 public class ArticleActivity extends AppCompatActivity {
 
@@ -28,5 +33,24 @@ public class ArticleActivity extends AppCompatActivity {
             }
         });
         webView.loadUrl(article.getWebUrl());
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_article, menu);
+
+        MenuItem item = menu.findItem(R.id.menu_item_share);
+        ShareActionProvider miShare = (ShareActionProvider) MenuItemCompat.getActionProvider(item);
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        shareIntent.setType("text/plain");
+
+        // get reference to WebView
+        WebView wvArticle = (WebView) findViewById(R.id.wvArticle);
+        // pass in the URL currently being used by the WebView
+        shareIntent.putExtra(Intent.EXTRA_TEXT, wvArticle.getUrl());
+
+        miShare.setShareIntent(shareIntent);
+        return super.onCreateOptionsMenu(menu);
     }
 }
